5bcb98e8933d0eeef89945758b872b08fc1327c3
Niket Bhalerao <114522379+bniket2107@users.noreply.github.com>
2025-01-21 13:27:49 +0530

    Update ProductAPI.js

--- client/src/api/ProductAPI.js
+++ client/src/api/ProductAPI.js
@@ -1,1 +1,23 @@
 
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const ProductAPI = () => {
+
+    const [products,setProducts]=useState([])
+
+    const getProducts = async()=> {
+        const res = await axios.get('/api/products')
+        setProducts(res.data.products)
+    }
+
+    useEffect(()=> {
+        getProducts()
+    },[])
+
+  return {
+    products : [products,setProducts]
+  }
+}
+
+export default ProductAPI
